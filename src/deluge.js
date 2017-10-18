519ef5edc4b926de9ffe31245e74f9a33542d7e5
Zonarius <zonarius@live.at>
2017-10-18 18:44:55 +0200

    Update deluge promise

--- src/deluge.js
+++ src/deluge.js
@@ -6,7 +6,11 @@ const deluge = Deluge(config.deluge.url, config.deluge.password);
 function addNyaaTorrent(id, path) {
   console.log(`Downloading torrent ${id} to ${path}`);
   const torrentUrl = `${config.nyaaUrl}/download/${id}.torrent`;
-  return deluge.add(torrentUrl, path);
+  return deluge.add(torrentUrl, {
+    download_location: '/data',
+    move_completed: true,
+    move_completed_path: path
+  });
 }
 
 module.exports = {
